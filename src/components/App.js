ed1a315353463a64ca2e2e30685c3f1f4081b41d
Alan Perez <alanp9six@gmail.com>
2019-07-09 06:20:40 -0500

    refactored to a functional component that uses useState hook

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,16 @@
 import React, { useState } from 'react'
 
-class App extends React.Component {
-  state = {
-    resource: 'posts',
-  }
-  render() {
+ const App = () =>  {
+    const [resource, setResource ] = useState('posts')
     return (
-
     <div>
       <div>
-        <button onClick={() => this.setState({resource: 'posts'})}>Posts</button>
-        <button onClick={() => this.setState({resource: 'todos'})}>Todos</button>  
+        <button onClick={() => setResource('posts')}>Posts</button>
+        <button onClick={() => setResource('todos')}>Todos</button>  
       </div>      
-      {this.state.resource}
+      {resource}
     </div>
       )
   }
-}
 
 export default App
