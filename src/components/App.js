fcbde74fb210e650bc8d698305d0284c34c94f7f
Alan Perez <alanp9six@gmail.com>
2019-07-09 09:43:17 -0500

    understand how hooks are used and how custom hooks can be reusable

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react'
 import ResourceList from './ResourceList'
-
+import UserList from './UserList'
  const App = () =>  {
     const [resource, setResource ] = useState('posts')
     return (
     <div>
+      <UserList />
       <div>
         {/* on click  run a function that will call setResource and updates it to posts or todos */}
         <button onClick={() => setResource('posts')}>Posts</button>
--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,25 +1,10 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
+import React from 'react'
+import useResources from './useResources'
+
 const ResourceList = ({ resource }) => {
-  // 
-  const [resources, setResources] = useState([]) // useState([]) initiliazed state to be an empty array
- // resources - access to initial state, can change it with setResources function
-
-
- // Call fetchResource any time ResourceList first gets rendered or recieves new value for resource
-  const fetchResource = async (resource) => {
-    // request
-    const response = await axios.get(`https://jsonplaceholder.typicode.com/${resource}`)
-    
-    //calls set state to update
-    setResources(response.data)
-  }
-
-  useEffect(() => {
-    fetchResource(resource)
-  }, [resource])
+  const resources = useResources(resource)
   
-    return <div>{resources.length}</div>
+    return <ul><div>{resources.map(record => <li key={record.id}>{record.title}</li>)}</div></ul>
 
 }
 export default ResourceList
